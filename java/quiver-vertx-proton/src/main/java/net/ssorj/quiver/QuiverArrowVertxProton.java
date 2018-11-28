2b27ba383bcb203999c5031d275484f480c698e1
Robbie Gemmell <robbie@apache.org>
2018-11-28 18:15:49 +0000

    WIP on adding server support for the vertx-proton arrow.
    - Incomplete, only basic testing. Have not tried timed durations.
    - May need quiver support changes for one use case, of a server that sends to a client receiver.

--- java/quiver-vertx-proton/src/main/java/net/ssorj/quiver/QuiverArrowVertxProton.java
+++ java/quiver-vertx-proton/src/main/java/net/ssorj/quiver/QuiverArrowVertxProton.java
@@ -27,27 +27,26 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.qpid.proton.amqp.Binary;
-import org.apache.qpid.proton.amqp.UnsignedLong;
 import org.apache.qpid.proton.amqp.messaging.Accepted;
 import org.apache.qpid.proton.amqp.messaging.ApplicationProperties;
 import org.apache.qpid.proton.amqp.messaging.Data;
 import org.apache.qpid.proton.message.Message;
 
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.proton.ProtonClient;
 import io.vertx.proton.ProtonConnection;
 import io.vertx.proton.ProtonReceiver;
 import io.vertx.proton.ProtonSender;
+import io.vertx.proton.ProtonServer;
 
 public class QuiverArrowVertxProton {
     private static final String CLIENT = "client";
+    private static final String SERVER = "server";
     private static final String ACTIVE = "active";
     private static final String RECEIVE = "receive";
     private static final String SEND = "send";
@@ -80,78 +79,154 @@ public class QuiverArrowVertxProton {
 
         final boolean durable = Arrays.asList(flags).contains("durable");
 
-        if (!CLIENT.equalsIgnoreCase(connectionMode)) {
-            throw new RuntimeException("This impl currently supports client mode only");
-        }
-
-        if (!ACTIVE.equalsIgnoreCase(channelMode)) {
-            throw new RuntimeException("This impl currently supports active mode only");
-        }
-
         if (transactionSize > 0) {
             throw new RuntimeException("This impl doesn't support transactions");
         }
 
-        final boolean sender;
+        final boolean clientMode;
+        if (CLIENT.equalsIgnoreCase(connectionMode)) {
+            clientMode = true;
+        } else if (SERVER.equalsIgnoreCase(connectionMode)) {
+            clientMode = false;
+        } else {
+            throw new java.lang.IllegalStateException("Unknown mode: " + connectionMode);
+        }
 
+        final boolean activeMode = ACTIVE.equalsIgnoreCase(channelMode);
+        if (clientMode && !activeMode) {
+            throw new RuntimeException("The client mode currently supports active mode only");
+        } else if (!clientMode && activeMode) {
+            throw new RuntimeException("The server mode currently supports passive mode only");
+        }
+
+        final boolean sendMode;
         if (SEND.equalsIgnoreCase(operation)) {
-            sender = true;
+            sendMode = true;
         } else if (RECEIVE.equalsIgnoreCase(operation)) {
-            sender = false;
+            sendMode = false;
         } else {
             throw new java.lang.IllegalStateException("Unknown operation: " + operation);
         }
 
         final CountDownLatch completionLatch = new CountDownLatch(1);
         final Vertx vertx = Vertx.vertx(new VertxOptions().setPreferNativeTransport(true));
-        final ProtonClient client = ProtonClient.create(vertx);
 
+        if(clientMode) {
+            handleClientMode(id, host, port, path, desiredDuration, desiredCount, bodySize, creditWindow, durable, sendMode, completionLatch, vertx);
+        } else {
+            handleServerMode(id, host, port, path, desiredDuration, desiredCount, bodySize, creditWindow, durable, sendMode, completionLatch, vertx);
+        }
+
+        // Await the operations completing, then shut down the Vertx instance.
+        completionLatch.await();
+
+        vertx.close();
+    }
+
+    private static void handleClientMode(final String id, final String host, final int port, final String path,
+                                         final int desiredDuration, final int desiredCount, final int bodySize, final int creditWindow,
+                                         final boolean durable, final boolean sendMode, final CountDownLatch completionLatch, final Vertx vertx) {
+        ProtonClient client = ProtonClient.create(vertx);
         client.connect(host, port, (res) -> {
                 if (res.succeeded()) {
                     final ProtonConnection connection = res.result();
 
                     connection.setContainer(id);
                     connection.closeHandler(x -> {
-                            completionLatch.countDown();
-                        });
+                        completionLatch.countDown();
+                    });
 
                     if (desiredDuration > 0) {
                         vertx.setTimer(desiredDuration * 1000, timerId -> {
-                                connection.close();
-                            });
+                            connection.close();
+                        });
                     }
 
-                    if (sender) {
-                        send(connection, path, desiredCount, bodySize, durable);
+                    connection.open();
+
+                    if (sendMode) {
+                        ProtonSender sender = connection.createSender(path);
+                        send(true, connection, sender, desiredCount, bodySize, durable);
                     } else {
-                        receive(connection, path, desiredCount, creditWindow);
+                        ProtonReceiver receiver = connection.createReceiver(path);
+                        receive(true, connection, receiver, desiredCount, creditWindow);
                     }
                 } else {
                     res.cause().printStackTrace();
                     completionLatch.countDown();
                 }
             });
-
-        // Await the operations completing, then shut down the Vertx
-        // instance.
-        completionLatch.await();
-
-        vertx.close();
     }
 
-    private static BufferedWriter getWriter() {
-        return new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static void handleServerMode(final String id, final String host, final int port, final String path,
+                                         final int desiredDuration, final int desiredCount, final int bodySize, final int creditWindow,
+                                         final boolean durable, final boolean sendMode, final CountDownLatch completionLatch, final Vertx vertx) {
+        ProtonServer server = ProtonServer.create(vertx);
+        // Configure how new connections are handled
+        server.connectHandler((connection) -> {
+            if (desiredDuration > 0) {
+                vertx.setTimer(desiredDuration * 1000, timerId -> {
+                    // TODO: No way to unbind-only. Not nice to remotely-close or sever the client connection.
+                    // Just awaiting the client closing connection currently (see below).
+                });
+            }
+
+            connection.openHandler(res -> {
+                connection.open();
+              });
+
+              connection.closeHandler(c -> {
+                connection.close();
+                connection.disconnect();
+
+                server.close(x -> {
+                    completionLatch.countDown();
+                });
+              });
+
+              connection.disconnectHandler(c -> {
+                connection.disconnect();
+              });
+
+              connection.sessionOpenHandler(session -> {
+                session.closeHandler(x -> {
+                  session.close();
+                  session.free();
+                });
+                session.open();
+              });
+
+              connection.senderOpenHandler(sender -> {
+                  send(false, connection, sender, desiredCount, bodySize, durable);
+              });
+
+              connection.receiverOpenHandler(receiver -> {
+                  receive(false, connection, receiver, desiredCount, creditWindow);
+              });
+        });
+
+        server.listen(port, (res) -> {
+          if (!res.succeeded()) {
+            System.err.println("Failed to start listening on port " + port + ":");
+            res.cause().printStackTrace();
+            completionLatch.countDown();
+          }
+        });
     }
 
-    private static void send(final ProtonConnection connection, final String address,
+    private static void send(final boolean client, final ProtonConnection connection, final ProtonSender sender,
                              final int desiredCount, final int bodySize, final boolean durable) {
         final StringBuilder line = new StringBuilder();
         final BufferedWriter out = getWriter();
         final AtomicInteger count = new AtomicInteger(0);
-        final ProtonSender sender = connection.createSender(address);
         final byte[] body = new byte[bodySize];
 
         Arrays.fill(body, (byte) 120);
+
+        if(!client) {
+            sender.setSource(sender.getRemoteSource());
+        }
 
         sender.sendQueueDrainHandler((s) -> {
                 try {
@@ -160,7 +235,10 @@ public class QuiverArrowVertxProton {
                             int sent = count.get();
 
                             if (sent > 0 && sent == desiredCount) {
-                                connection.close();
+                                if(client) {
+                                    // Not for servers, to avoid remote-closing the 'client' connection.
+                                    connection.close();
+                                }
                                 break;
                             }
 
@@ -193,17 +271,19 @@ public class QuiverArrowVertxProton {
                 }
             });
 
-        connection.open();
         sender.open();
     }
 
-    private static void receive(final ProtonConnection connection, final String address,
+    private static void receive(final boolean client, final ProtonConnection connection, final ProtonReceiver receiver,
                                 final int desiredCount, final int creditWindow) {
         final StringBuilder line = new StringBuilder();
         final BufferedWriter out = getWriter();
         final AtomicInteger count = new AtomicInteger(0);
         final int creditTopUpThreshold = Math.max(1, creditWindow / 2);
-        final ProtonReceiver receiver = connection.createReceiver(address);
+
+        if(!client) {
+            receiver.setTarget(receiver.getRemoteTarget());
+        }
 
         receiver.setAutoAccept(false).setPrefetch(0).flow(creditWindow);
         receiver.handler((delivery, msg) -> {
@@ -225,7 +305,10 @@ public class QuiverArrowVertxProton {
                         }
 
                         if (count.incrementAndGet() == desiredCount) {
-                            connection.close();
+                            if(client) {
+                                // Not for servers, to avoid remote-closing the 'client' connection.
+                                connection.close();
+                            }
                         }
                     } finally {
                         out.flush();
@@ -235,7 +318,10 @@ public class QuiverArrowVertxProton {
                 }
             });
 
-        connection.open();
         receiver.open();
+    }
+
+    private static BufferedWriter getWriter() {
+        return new BufferedWriter(new OutputStreamWriter(System.out));
     }
 }
